a7ecf5c9d6c064003f64d7ad84ecf85a397bcd07
Erik Unevik <erikunevik@gmail.com>
2025-08-28 19:28:53 +0200

    Lecture 07 2025-08-28

---
+++ 07/worksheets_snowflake/setup_user_role.sql
@@ -0,0 +1,35 @@
+-- NOTE: you should place the create user part 
+-- in a separate file and .gitignore it as it contains credentials
+
+
+-- create dlt user and dlt role 
+USE ROLE USERADMIN;
+
+CREATE ROLE IF NOT EXISTS job_ads_dlt_role;
+
+CREATE USER IF NOT EXISTS extract_loader2
+    PASSWORD = 'KJ8vemQ3bk!!!!' -- create a password and fill in here
+    DEFAULT_WAREHOUSE = dev_wh;
+
+
+-- grant role to user
+USE ROLE SECURITYADMIN;
+
+GRANT ROLE job_ads_dlt_role TO USER extract_loader2;
+
+-- grant privileges to role
+GRANT USAGE ON WAREHOUSE dev_wh TO ROLE job_ads_dlt_role;
+GRANT USAGE ON DATABASE job_ads TO ROLE job_ads_dlt_role;
+GRANT USAGE ON SCHEMA job_ads.staging TO ROLE job_ads_dlt_role;
+GRANT CREATE TABLE ON SCHEMA job_ads.staging TO ROLE job_ads_dlt_role;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA job_ads.staging TO ROLE job_ads_dlt_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON FUTURE TABLES IN SCHEMA job_ads.staging TO ROLE job_ads_dlt_role;
+
+-- check grants
+SHOW GRANTS ON SCHEMA job_ads.staging;
+SHOW FUTURE GRANTS IN SCHEMA job_ads.staging;
+SHOW GRANTS TO ROLE job_ads_dlt_role;
+SHOW GRANTS TO USER extract_loader2;
+
+GRANT ROLE job_ads_dlt_role TO USER ERIKUNEVIK;
